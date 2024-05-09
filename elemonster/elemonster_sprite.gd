1f7e40ffbd136f76c5a1168cead8ed8bb096ef35
xorblo-doitus <blabla86420@gmail.com>
2024-05-09 18:04:03 +0200

    Added ElemonsterSprite

---
+++ elemonster/elemonster_sprite.gd
@@ -0,0 +1,38 @@
+class_name ElemonsterSprite
+extends Node2D
+
+
+@export var elemonster_resource: ElemonsterResource:
+	set(new):
+		elemonster_resource = new
+		update_elemonster_resource()
+
+
+@onready var shadow_sprite: Sprite2D = $Shadow/ShadowSprite
+@onready var monster_sprite: Sprite2D = $Monster/MonsterSprite
+
+
+func _ready() -> void:
+	update_elemonster_resource()
+
+
+func update_elemonster_resource() -> void:
+	update_sprite()
+	update_shadow()
+
+
+func update_shadow() -> void:
+	if not is_node_ready():
+		return
+	
+	shadow_sprite.modulate = elemonster_resource.shadow_color
+	shadow_sprite.texture = elemonster_resource.shadow_texture
+	shadow_sprite.scale = elemonster_resource.shadow_size
+
+
+func update_sprite() -> void:
+	if not is_node_ready():
+		return
+	
+	monster_sprite.texture = elemonster_resource.sprite_texture
+	monster_sprite.position.y = 16 - elemonster_resource.sprite_texture.get_size().y/2.0
