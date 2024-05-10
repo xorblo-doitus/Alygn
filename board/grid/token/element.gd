9caff3c0d858fc0f8f2ba15995c06afc25a1274e
xorblo-doitus <blabla86420@gmail.com>
2024-05-10 13:22:14 +0200

    Added element advantages

--- board/grid/token/element.gd
+++ board/grid/token/element.gd
@@ -12,7 +12,37 @@ enum Type {
 	ANY = (1 << MAX_TYPE_INDEX) - 1,
 }
 const MAX_TYPE_INDEX: int = 4
+const MAX_TYPE_WITH_ADVANTAGE_INDEX: int = MAX_TYPE_INDEX - 1
 
+
+static var advantages: Array[PackedFloat64Array] = []
+
+
+static func _static_init() -> void:
+	var file := FileAccess.open("res://board/grid/token/elements.csv", FileAccess.READ)
+	file.get_csv_line()
+	
+	for __ in MAX_TYPE_WITH_ADVANTAGE_INDEX:
+		var line := file.get_csv_line()
+		var advantages_line: PackedFloat64Array = PackedFloat64Array()
+		advantages_line.resize(MAX_TYPE_WITH_ADVANTAGE_INDEX)
+		
+		for i_column in MAX_TYPE_WITH_ADVANTAGE_INDEX:
+			advantages_line[i_column] = 1 + float(line[i_column + 1])
+		
+		advantages.push_back(advantages_line)
+
+
+static func get_avantage(of: Type, on: Type) -> float:
+	var result = 1
+	
+	for of_type in MAX_TYPE_WITH_ADVANTAGE_INDEX:
+		if (1 << of_type) & of:
+			for on_type in MAX_TYPE_WITH_ADVANTAGE_INDEX:
+				if (1 << on_type) & on:
+					result *= advantages[of_type][on_type]
+	
+	return result
 
 
 static func get_color(for_type: Type) -> Color:
